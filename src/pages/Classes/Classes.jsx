30bd3dd67c63cab00d0b0341d1bb86c16f0cb5be
unknown <anikm2982@gmail.com>
2023-10-09 14:14:01 +0600

    all product updated

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -50,7 +50,7 @@ const Classes = () => {
     return (
         <>
             <div className='md:p-10 my-10 mx-auto w-11/12'>
-                <p className='text-3xl font-bold mb-10 text-center'>All Classes</p>
+                <p className='text-3xl font-bold mb-10 text-center'>All Jewelry</p>
                 <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-6'>
                     {allClasses.map((classes) =>
                         <div key={classes._id} className={`card w-full group glass ${classes.seats === 0 && 'bg-red-600'}`}>
@@ -58,16 +58,15 @@ const Classes = () => {
                                 <img className='w-full h-80 group-hover:scale-110' src={classes.image} alt="card" />
                             </figure>
                             <div className="card-body">
-                                <p className='font-semibold'>Class name: <span className='font-normal'>{classes.className}</span></p>
-                                <p className='font-semibold'>Instructor Name: <span className='font-normal'>{classes.instructorName}</span></p>
-                                <p className='font-semibold'>Available Seats: <span className='font-normal'>{classes.seats}</span></p>
+                                <p className='font-semibold'>Jewelry Name: <span className='font-normal'>{classes.className}</span></p>
+                                <p className='font-semibold'>Available Product: <span className='font-normal'>{classes.seats}</span></p>
                                 <p className='font-semibold'>Price: <span className='font-normal'>${classes.price}</span></p>
                                 <button
                                     disabled={classes.seats === 0 || currentUser.role === 'admin' || currentUser.role === 'instructor'}
                                     onClick={() => handleSelect(classes)}
                                     className="button mx-auto"
                                 >
-                                    Enroll
+                                    Add to Cart
                                 </button>
                             </div>
                         </div>
