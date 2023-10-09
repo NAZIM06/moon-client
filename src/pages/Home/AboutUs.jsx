00cb6b4e7a2b74c243dba7c2263d09d7535e9f3c
unknown <anikm2982@gmail.com>
2023-10-09 14:10:59 +0600

    home done

--- src/pages/Home/AboutUs.jsx
+++ src/pages/Home/AboutUs.jsx
@@ -11,28 +11,29 @@ const AboutUs = () => {
     <div>
       <div className="max-w-screen-lg mx-auto my-8 pt-12">
         <div className="text-center">
-          <h1 className='text-5xl font-extrabold mb-5 text-blue-900'>What Will you learn?</h1>
+          <h1 className='text-5xl font-extrabold mb-5 text-blue-900'>What Will you Get?</h1>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-5" data-aos="zoom-in">
           <div className="rounded-lg shadow-md p-4 text-center bg-slate-50">
-            <img src="https://i.ibb.co/RHLpJxm/megaphone-icon-0-removebg-preview.png" alt="Logo" className="w-20 h-20 mx-auto" />
-            <h2 className="text-lg text-blue-700 font-bold mb-2">Speak</h2>
-            <p className="text-gray-500">Speak with native speakers on any topic you want (not just about the weather). Make friends in different parts of the Earth!.</p>
+            <img src="https://static.vecteezy.com/system/resources/previews/011/054/455/large_2x/network-protection-icons-lock-security-icon-vector.jpg" alt="Logo" className="w-20 h-20 mx-auto" />
+            <h2 className="text-lg text-blue-700 font-bold mb-2">SAFE DELIVERY</h2>
+            <p className="text-gray-500">Pay with the world’s most popular, secure payment.</p>
           </div>
           <div className="bg-slate-50 rounded-lg shadow-md p-4 text-center">
-            <img src="https://i.ibb.co/xS8JFVg/images-removebg-preview.png" alt="Logo" className="w-20 h-20 mx-auto" />
-            <h2 className="text-lg text-blue-700 font-bold mb-2">Read</h2>
-            <p className="text-gray-500">Read literature in the original language. Grow your vocabulary, master the language’s grammar, and broaden your horizons!</p>
+            <img src="https://cdn-icons-png.flaticon.com/512/8589/8589772.png?ga=GA1.1.1168090119.1679595557" alt="Logo" className="w-20 h-20 mx-auto" />
+            <h2 className="text-lg text-blue-700 font-bold mb-2">FAST SHIPPING</h2>
+            <p className="text-gray-500">
+              With sites in 5 languages, ship to over 200 countries.</p>
           </div>
           <div className="bg-slate-50 rounded-lg shadow-md p-4 text-center">
-            <img src="https://i.ibb.co/j43FxyZ/images-removebg-preview-1.png" alt="Logo" className="w-20 h-20 mx-auto" />
-            <h2 className="text-lg text-blue-700 font-bold mb-2">Understand</h2>
-            <p className="text-gray-500">Understand new things about the foreign country, its culture and traditions. Feel yourself like a piece of something global!</p>
+            <img src="https://cdn-icons-png.flaticon.com/512/7133/7133348.png" alt="Logo" className="w-20 h-20 mx-auto" />
+            <h2 className="text-lg text-blue-700 font-bold mb-2">Buyer discount</h2>
+            <p className="text-gray-500">Special Offer Every Month from Seller.</p>
           </div>
           <div className="bg-slate-50 rounded-lg shadow-md p-4 text-center">
-            <img src="https://i.ibb.co/Nn9DkGS/images-removebg-preview-2.png" alt="Logo" className="w-20 h-20 mx-auto" />
-            <h2 className="text-lg text-blue-700 font-bold mb-2">Write</h2>
-            <p className="text-gray-500">Write to your foreign friends or conduct business correspondence with people from other countries and even continents!</p>
+            <img src="https://www.brikkapp.com/media/platforms/logos/181_Profitus_Logo_Lithuania-200x200.png?token=47ba157a8f02b8a478472154e90914fa" alt="Logo" className="w-20 h-20 mx-auto" />
+            <h2 className="text-lg text-blue-700 font-bold mb-2">365 DAYS RETURN</h2>
+            <p className="text-gray-500">Round-the-clock assistance for a shopping experience.</p>
           </div>
         </div>
       </div>
