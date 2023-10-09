d8e959a1e4eb7670c64fd19693503a9fbc49b06b
unknown <anikm2982@gmail.com>
2023-10-09 13:53:46 +0600

    banner edited

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -24,10 +24,20 @@ const Banner = () => {
 
         <div>
           <img src="http://ledthanhdat.vn/html/kaycee/assets/images/slide62.jpg" alt="Slide 2" />
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center md:w-3/4 lg:w-1/2">
+            <Fade><p className="text-blue-900 text-2xl md:text-6xl font-bold mb-3">Modern Ring</p>
+              <p className="md:text-xl text-black font-bold hidden sm:block">Stock <span className='font-bold text-blue-900'>Limit</span> In</p></Fade>
+            <button className="btn bg-blue-900 text-white hover:text-black font-medium mx-auto btn-sm md:btn-md lg:btn-lg mt-3 md:mt-5">Buy Now</button>
+          </div>
 
         </div>
         <div>
           <img src="http://ledthanhdat.vn/html/kaycee/assets/images/slide63.jpg" alt="Slide 3" />
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center md:w-3/4 lg:w-1/2">
+            <Fade><p className="text-blue-900 text-2xl md:text-6xl font-bold mb-3">New Arrivals</p>
+              <p className="md:text-xl text-black font-bold hidden sm:block">Get <span className='font-bold text-blue-900'>25%</span> Off</p></Fade>
+            <button className="btn bg-blue-900 text-white hover:text-black font-medium mx-auto btn-sm md:btn-md lg:btn-lg mt-3 md:mt-5">Buy Now</button>
+          </div>
 
         </div>
       </Carousel>
