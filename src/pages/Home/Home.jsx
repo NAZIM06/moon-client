1183398651d20e3a92d3e20a8997724f3ff9945d
unknown <anikm2982@gmail.com>
2023-10-09 12:42:06 +0600

    home updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import AboutUs from './AboutUs';
-import PopularInstructors from './PopularInstructors/PopularInstructors';
+
 import PopularClasses from './PopularClasses/PopularClasses';
 
 const Home = () => {
   return (
     <div className='py-6'>
       <Banner></Banner>
-      <div className='py-20'><PopularInstructors /></div>
+      <div className='py-20'>
       <PopularClasses />
-      <AboutUs></AboutUs>
+      <AboutUs></AboutUs></div>
     </div>
   );
 };
